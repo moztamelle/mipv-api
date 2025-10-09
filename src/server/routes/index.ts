3c74d71cdd11b7ebcc324435fe29a919f49a9023
Moztamelle <moztamelle@gmail.com>
2025-10-09 10:11:20 +0200

    feat: clean project

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,12 +1,2 @@
-import routerCollaborator from './collaborator'
-import routerEvents from './events'
-import routerGuests from './guest'
-import routerInvites from './invites'
-import routerTables from './tables'
 export default {
-  routerEvents,
-  routerTables,
-  routerGuests,
-  routerCollaborator,
-  routerInvites,
 }
--- src/server/routes/notifications.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { authentication } from '../authentication/middlewares'
-import { findAll } from '../controllers/notifications'
-
-const routerNotification = Router()
-
-routerNotification.get('/', [authentication], findAll)
-
-export default routerNotification
